1de54a393da309e39f44ed57516e739e212ca766
AhmadFauzanZW <ahfa201204@gmail.com>
2025-02-12 21:54:39 +0700

    store extracted data to data.txt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests as req
 import selectorlib
+import time
 
 URL = "https://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
@@ -16,8 +17,27 @@ def extract(source):
     value = extractor.extract(source)['tours']
     return value
 
+def store(extracted):
+    with open("data.txt", 'a') as file:
+        file.write(extracted + '\n')
+
+def read(extracted):
+    with open("data.txt", 'r') as file:
+        return file.read()
+
+def send_mail():
+    print("Email berhasil dikirim!")
+
 if __name__ == "__main__":
-    scraped = scrape(URL)
-    extracted = extract(scraped)
-    print(extracted)
+    while True:
+        scraped = scrape(URL)
+        extracted = extract(scraped)
+        print(extracted)
+
+        content = read(extracted)
+        if extracted != "No upcoming tours":
+            if extracted not in content:
+                store(extracted)
+                send_mail()
+        time.sleep(5)
 
